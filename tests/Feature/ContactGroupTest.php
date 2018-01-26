f18ae219461a0ebe8ef9596a8a72d22a287cf57f
Alexander Glück <github@alexanderglueck.at>
2018-01-26 23:39:54 +0100

    Add more contact and contact group tests

--- tests/Feature/ContactGroupTest.php
+++ tests/Feature/ContactGroupTest.php
@@ -24,6 +24,8 @@ class ContactGroupTest extends TestCase
             'updated_by' => $user->id
         ]);
 
+        $this->assertDatabaseMissing('contact_groups', $contactGroup->toArray());
+
         $response = $this
             ->actingAs($user)
             ->post(route('contact_groups.store'), $contactGroup->toArray());
@@ -31,6 +33,98 @@ class ContactGroupTest extends TestCase
         $response->assertStatus(302);
         $response->assertSessionMissing('errors');
         $this->assertDatabaseHas('contact_groups', $contactGroup->toArray());
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_view_a_contact_group()
+    {
+        $user = factory(User::class)->create();
+        $contactGroup = factory(ContactGroup::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('contact_groups.show', [$contactGroup->slug]));
+
+        $response->assertStatus(200);
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_view_the_contact_group_delete_view()
+    {
+        $user = factory(User::class)->create();
+        $contactGroup = factory(ContactGroup::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('contact_groups.delete', [$contactGroup->slug]));
+
+        $response->assertStatus(200);
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_delete_a_contact_group()
+    {
+        \Session::start();
+
+        $user = factory(User::class)->create();
+        $contactGroup = factory(ContactGroup::class)->create();
+
+        $this->assertDatabaseHas('contact_groups', $contactGroup->toArray());
+
+        $response = $this->actingAs($user)
+            ->delete(route('contact_groups.destroy', [$contactGroup->slug]), [
+                '_token' => csrf_token()
+            ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionMissing('alert-danger');
+        $this->assertDatabaseMissing('contact_groups', $contactGroup->toArray());
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_view_the_contact_group_edit_view()
+    {
+        $user = factory(User::class)->create();
+        $contactGroup = factory(ContactGroup::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('contact_groups.edit', [$contactGroup->slug]));
+
+        $response->assertStatus(200);
+        $response->assertSee('Kontaktgruppe bearbeiten');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_update_a_contact_group()
+    {
+        \Session::start();
+
+        $user = factory(User::class)->create();
+        $contactGroup = factory(ContactGroup::class)->create();
+
+        $check = $contactGroup->toArray();
+
+        $contactGroup2 = $contactGroup;
+        $contactGroup2->name = "something else";
+
+        unset($contactGroup2->updated_at);
+        unset($contactGroup2->updated_by);
+        unset($contactGroup->updated_at);
+        unset($contactGroup->updated_by);
+
+        $response = $this->actingAs($user)
+            ->put(route('contact_groups.update', [$contactGroup->slug]),
+                array_merge($contactGroup2->toArray(),
+                    ['_token' => csrf_token()]
+                ));
+
+        $response->assertStatus(302);
+        $response->assertSessionMissing('alert-danger');
+        $this->assertDatabaseHas('contact_groups', $contactGroup2->toArray());
+        $this->assertDatabaseMissing('contact_groups', $check);
         $this->assertAuthenticatedAs($user);
     }
 }
--- tests/Feature/ContactTest.php
+++ tests/Feature/ContactTest.php
@@ -36,4 +36,129 @@ class ContactTest extends TestCase
         $this->assertDatabaseHas('contacts', $contact->toArray());
         $this->assertAuthenticatedAs($user);
     }
+
+    /** @test */
+    public function a_user_can_view_a_contact_created_by_him()
+    {
+        $user = factory(User::class)->create();
+        $contact = factory(Contact::class)->create([
+            'created_by' => $user->id
+        ]);
+
+        $response = $this->actingAs($user)
+            ->get(route('contacts.show', [$contact->slug]));
+
+        $response->assertStatus(200);
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_not_view_a_contact_created_by_another_user()
+    {
+        $user = factory(User::class)->create();
+        $contact = factory(Contact::class)->create([
+            'created_by' => $user->id
+        ]);
+
+        $viewer = factory(User::class)->create();
+
+        $response = $this->actingAs($viewer)
+            ->get(route('contacts.show', [$contact->slug]));
+
+        $response->assertStatus(403);
+        $this->assertAuthenticatedAs($viewer);
+
+        $response = $this->actingAs($user)
+            ->get(route('contacts.show', [$contact->slug]));
+
+        $response->assertStatus(200);
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_view_the_contact_delete_view()
+    {
+        $user = factory(User::class)->create();
+        $contact = factory(Contact::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('contacts.delete', [$contact->slug]));
+
+        $response->assertStatus(200);
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_delete_a_contact()
+    {
+        \Session::start();
+
+        $user = factory(User::class)->create();
+        $contact = factory(Contact::class)->create();
+
+        $this->assertDatabaseHas('contacts', $contact->toArray());
+
+        $response = $this->actingAs($user)
+            ->delete(route('contacts.destroy', [$contact->slug]), [
+                '_token' => csrf_token()
+            ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionMissing('alert-danger');
+        $this->assertDatabaseMissing('contacts', $contact->toArray());
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_view_the_contact_edit_view()
+    {
+        $user = factory(User::class)->create();
+        $contact = factory(Contact::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get(route('contacts.edit', [$contact->slug]));
+
+        $response->assertStatus(200);
+        $response->assertSee('Kontakt bearbeiten');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    /** @test */
+    public function a_user_can_update_a_contact()
+    {
+        \Session::start();
+
+        $user = factory(User::class)->create();
+        $contactGroup = factory(Contact::class)->create();
+
+        $check = [
+            'id' => $contactGroup->id,
+            'lastname' => $contactGroup->lastname,
+        ];
+
+        $contactGroup2 = $contactGroup;
+        $contactGroup2->lastname = "something else";
+        $contactGroup2->date_of_birth = $contactGroup2->formatted_date_of_birth;
+
+        $parameters = $contactGroup2->toArray();
+        $parameters['date_of_birth'] = $contactGroup2->formatted_date_of_birth;
+
+        unset($parameters['updated_at']);
+        unset($contactGroup2['updated_at']);
+        unset($parameters['updated_by']);
+        unset($contactGroup2['updated_by']);
+
+        $response = $this->actingAs($user)
+            ->put(route('contacts.update', [$contactGroup->slug]),
+                array_merge($parameters,
+                    ['_token' => csrf_token()]
+                ));
+
+
+        $response->assertStatus(302);
+        $response->assertSessionMissing('alert-danger');
+        $this->assertDatabaseHas('contacts', $contactGroup2->toArray());
+        $this->assertDatabaseMissing('contacts', $check);
+        $this->assertAuthenticatedAs($user);
+    }
 }
