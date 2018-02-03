8cffc7f70d909012f7969ef531eb74d2fedfdcca
Alexander Glück <github@alexanderglueck.at>
2018-02-03 10:57:45 +0100

    Add the generated css/js/font files to the gitignore

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -24,6 +24,15 @@
 </head>
 
 <body class="gray-bg">
+<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+    <h5 class="my-0 mr-md-auto font-weight-normal">{{ config('app.name') }}</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="{{ route('login') }}">Sign in</a>
+    </nav>
+    <a class="btn btn-outline-primary" href="{{ route('register') }}">Sign
+        up</a>
+</div>
+
 @yield('content')
 
 <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
