5e33c525b5829bbad90c0a2c08524462f71ae1a7
Alexander Glück <github@alexanderglueck.at>
2018-04-21 13:15:57 +0200

    Extract validation from CommentController

---
+++ app/Http/Requests/Comment/DeleteComment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Comment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DeleteComment extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('delete comments');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ app/Http/Requests/Comment/StoreComment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests\Comment;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreComment extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('create comments');;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'comment' => 'required'
+        ];
+    }
+}
