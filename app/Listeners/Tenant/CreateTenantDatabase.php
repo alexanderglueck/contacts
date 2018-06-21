2fe926ba3e763a3767d3ee70d3505c2b66d0f457
Alexander Glück <github@alexanderglueck.at>
2018-06-21 22:30:17 +0200

    StyleCI fixes

--- app/Listeners/Tenant/CreateTenantDatabase.php
+++ app/Listeners/Tenant/CreateTenantDatabase.php
@@ -2,12 +2,11 @@
 
 namespace App\Listeners\Tenant;
 
-use App\ContactIndexConfigurator;
 use App\Models\Role;
-use App\Models\Team;
 use App\Models\User;
 use App\Models\Permission;
 use App\Tenant\Models\Tenant;
+use App\ContactIndexConfigurator;
 use App\Events\Auth\UserSignedUp;
 use App\Events\Tenant\TenantWasCreated;
 use Illuminate\Support\Facades\Artisan;
