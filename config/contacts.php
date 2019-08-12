ac13ee25b205013b29b8355272e3e5b32a35c757
Alexander Glück <github@alexanderglueck.at>
2019-08-12 20:14:46 +0200

    Cleanup

--- config/contacts.php
+++ config/contacts.php
@@ -7,9 +7,5 @@ return [
         'default' => 20,
         'activities' => 20
     ],
-    'tenant' => [
-        'prefix' => env('CONTACTS_TENANT_PREFIX', 'contact_'),
-        'system' => env('DB_DATABASE', 'contact_system')
-    ],
     'signup_enabled' => env('CONTACTS_SIGNUP_ENABLED', true)
 ];
